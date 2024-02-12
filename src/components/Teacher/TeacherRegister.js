371a54f57e0783410b8cdcde389a12400a5c7392
Saad Ali Khan <saadalikhan042@gmail.com>
2024-02-12 09:34:02 +0500

    teacher registration complete

--- src/components/Teacher/TeacherRegister.js
+++ src/components/Teacher/TeacherRegister.js
@@ -1,13 +1,67 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+const baseUrl = "http://localhost:8000/api";
 const TeacherRegister = () => {
+  const [teacherData, setTeacherData] = useState({
+    full_name: "",
+    email: "",
+    password: "",
+    phone_no: "",
+    qualification: "",
+    skills: "",
+    status: "",
+  });
+  const handleChange = (event) => {
+    setTeacherData({
+      ...teacherData,
+      [event.target.name]: event.target.value,
+    });
+    event.preventDefault();
+  };
+
+  const handleSubmit = () => {
+    const teacherFormData = new FormData();
+    teacherFormData.append("full_name", teacherData.full_name);
+    teacherFormData.append("email", teacherData.email);
+    teacherFormData.append("password", teacherData.password);
+    teacherFormData.append("phone_no", teacherData.phone_no);
+    teacherFormData.append("qualification", teacherData.qualification);
+    teacherFormData.append("skills", teacherData.skills);
+
+    try {
+      axios.post(baseUrl + "/teachers/", teacherFormData).then((response) => {
+        setTeacherData({
+          full_name: "",
+          email: "",
+          password: "",
+          phone_no: "",
+          qualification: "",
+          skills: "",
+          status: "success",
+        });
+      });
+      console.log(teacherData);
+    } catch (error) {
+      console.log(error);
+      setTeacherData({ status: "error" });
+    }
+  };
   useEffect(() => {
     document.title = "Teacher Registration";
   });
   return (
     <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-md">
+        {teacherData.status === "success" && (
+          <p className="text-center text-green-500">
+            User Successfuly Registered
+          </p>
+        )}
+        {teacherData.status === "error" && (
+          <p className="text-center text-red-500">Something went Wrong</p>
+        )}
         <img
           class="mx-auto h-10 w-auto"
           src="https://www.svgrepo.com/show/301692/login.svg"
@@ -30,70 +84,42 @@ const TeacherRegister = () => {
       <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <form method="POST" action="#">
-            <div>
+            <div class="mt-6">
               <label
-                for="email"
-                class="block text-sm font-medium leading-5  text-gray-700"
+                for="name"
+                class="block text-sm font-medium leading-5 text-gray-700"
               >
-                Full Name
+                Name
               </label>
-              <div class="mt-1 relative rounded-md shadow-sm">
+              <div class="mt-1 rounded-md shadow-sm">
                 <input
                   id="name"
-                  name="name"
-                  placeholder="John Doe"
+                  name="full_name"
+                  onChange={handleChange}
                   type="text"
                   required=""
-                  value=""
+                  value={teacherData.full_name}
                   class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                 />
-                <div class="hidden absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                  <svg
-                    class="h-5 w-5 text-red-500"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
               </div>
             </div>
-
             <div class="mt-6">
               <label
                 for="email"
-                class="block text-sm font-medium leading-5  text-gray-700"
+                class="block text-sm font-medium leading-5 text-gray-700"
               >
-                Email address
+                Email
               </label>
-              <div class="mt-1 relative rounded-md shadow-sm">
+              <div class="mt-1 rounded-md shadow-sm">
                 <input
                   id="email"
                   name="email"
-                  placeholder="user@example.com"
+                  onChange={handleChange}
                   type="email"
                   required=""
-                  value=""
-                  class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5
-              "
+                  value={teacherData.email}
+                  class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                 />
-                <div class="hidden absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                  <svg
-                    class="h-5 w-5 text-red-500"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
               </div>
             </div>
 
@@ -108,7 +134,9 @@ const TeacherRegister = () => {
                 <input
                   id="password"
                   name="password"
+                  onChange={handleChange}
                   type="password"
+                  value={teacherData.password}
                   required=""
                   class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                 />
@@ -116,107 +144,66 @@ const TeacherRegister = () => {
             </div>
             <div class="mt-6">
               <label
-                for="email"
-                class="block text-sm font-medium leading-5  text-gray-700"
+                for="phone"
+                class="block text-sm font-medium leading-5 text-gray-700"
               >
-                Phone No
+                Cell No
               </label>
-              <div class="mt-1 relative rounded-md shadow-sm">
+              <div class="mt-1 rounded-md shadow-sm">
                 <input
-                  id="email"
-                  name="email"
-                  placeholder=""
-                  type="email"
+                  id="phone"
+                  name="phone_no"
+                  onChange={handleChange}
+                  type="text"
                   required=""
-                  value=""
-                  class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5
-              "
+                  value={teacherData.phone_no}
+                  class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                 />
-                <div class="hidden absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                  <svg
-                    class="h-5 w-5 text-red-500"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
               </div>
             </div>
             <div class="mt-6">
               <label
-                for="email"
-                class="block text-sm font-medium leading-5  text-gray-700"
+                for="qualification"
+                class="block text-sm font-medium leading-5 text-gray-700"
               >
                 Qualification
               </label>
-              <div class="mt-1 relative rounded-md shadow-sm">
+              <div class="mt-1 rounded-md shadow-sm">
                 <input
-                  id="email"
-                  name="email"
-                  placeholder="Graduate"
-                  type="email"
+                  id="qualification"
+                  name="qualification"
+                  onChange={handleChange}
+                  type="text"
+                  value={teacherData.qualification}
                   required=""
-                  value=""
-                  class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5
-              "
+                  class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                 />
-                <div class="hidden absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                  <svg
-                    class="h-5 w-5 text-red-500"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
               </div>
             </div>
             <div class="mt-6">
               <label
-                for="email"
-                class="block text-sm font-medium leading-5  text-gray-700"
+                for="skills"
+                class="block text-sm font-medium leading-5 text-gray-700"
               >
                 Skills
               </label>
-              <div class="mt-1 relative rounded-md shadow-sm">
+              <div class="mt-1 rounded-md shadow-sm">
                 <input
-                  id="email"
-                  name="email"
-                  placeholder="JavaScript | React | Python"
-                  type="email"
+                  id="skills"
+                  name="skills"
+                  onChange={handleChange}
+                  type="text"
+                  value={teacherData.skills}
                   required=""
-                  value=""
-                  class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5
-              "
+                  class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                 />
-                <div class="hidden absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                  <svg
-                    class="h-5 w-5 text-red-500"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
               </div>
             </div>
             <div class="mt-6">
               <span class="block w-full rounded-md shadow-sm">
                 <button
                   type="submit"
+                  onClick={handleSubmit}
                   class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
                 >
                   Create account
