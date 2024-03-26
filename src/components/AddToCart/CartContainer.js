5fa7610f36efeea0c6bfbb42e022528dcf4760b9
Saad Ali Khan <saadalikhan042@gmail.com>
2024-03-26 18:35:42 +0500

    clear cart and remove item from cart complete

--- src/components/AddToCart/CartContainer.js
+++ src/components/AddToCart/CartContainer.js
@@ -1,38 +1,24 @@
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import { useDispatch, useSelector } from "react-redux";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import Courses from "../Courses";
-
-const products = [
-  {
-    id: 1,
-    name: "Throwback Hip Bag",
-    href: "#",
-    color: "Salmon",
-    price: "$90.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
-    imageAlt:
-      "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
-  },
-  {
-    id: 2,
-    name: "Medium Stuff Satchel",
-    href: "#",
-    color: "Blue",
-    price: "$32.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
-    imageAlt:
-      "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
-  },
-  // More products...
-];
+import { clearCart, removeItem } from "../../features/cart/cartSlice";
 
 export default function Example() {
+  const dispatch = useDispatch();
   const [open, setOpen] = useState(true);
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+  if (amount < 1) {
+    return (
+      <section className="flex font-serif items-center justify-center">
+        <header>
+          <h2 className="text-4xl mb-16">your bag</h2>
+          <h4 className="text-red-600">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
 
   return (
     <>
@@ -92,7 +78,7 @@ export default function Example() {
                               role="list"
                               className="-my-6 divide-y divide-gray-200"
                             >
-                              {products.map((product) => (
+                              {cartItems.map((product) => (
                                 <li key={product.id} className="flex py-6">
                                   <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                                     <img
@@ -125,6 +111,9 @@ export default function Example() {
                                         <button
                                           type="button"
                                           className="font-medium text-indigo-600 hover:text-indigo-500"
+                                          onClick={() =>
+                                            dispatch(removeItem(product.id))
+                                          }
                                         >
                                           Remove
                                         </button>
@@ -141,7 +130,7 @@ export default function Example() {
                       <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
                         <div className="flex justify-between text-base font-medium text-gray-900">
                           <p>Subtotal</p>
-                          <p>$262.00</p>
+                          <p>${total.toFixed(2)}</p>
                         </div>
                         <p className="mt-0.5 text-sm text-gray-500">
                           Shipping and taxes calculated at checkout.
@@ -160,9 +149,9 @@ export default function Example() {
                             <button
                               type="button"
                               className="font-medium text-indigo-600 hover:text-indigo-500"
-                              onClick={() => setOpen(false)}
+                              onClick={() => dispatch(clearCart())}
                             >
-                              Continue Shopping
+                              Clear Cart
                               <span aria-hidden="true"> &rarr;</span>
                             </button>
                           </p>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -4,7 +4,33 @@ import axios from "axios";
 const url = "http://localhost:8000/api/course/";
 
 const initialState = {
-  cartItems: [],
+  cartItems: [
+    {
+      id: 1,
+      name: "Throwback Hip Bag",
+      href: "#",
+      color: "Salmon",
+      price: "$90.00",
+      quantity: 1,
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
+      imageAlt:
+        "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
+    },
+    {
+      id: 2,
+      name: "Medium Stuff Satchel",
+      href: "#",
+      color: "Blue",
+      price: "$32.00",
+      quantity: 1,
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
+      imageAlt:
+        "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
+    },
+    // More products...
+  ],
   amount: 4,
   total: 0,
   isLoading: true,
@@ -25,6 +51,17 @@ export const getCartItems = createAsyncThunk(
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+  },
 });
+
+export const { clearCart, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
